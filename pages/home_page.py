4f24181b47fb08e5989ca9c09afc45faad44137a
Nelly <nellylisak6@gmail.com>
2024-11-20 22:19:23 +0200

    added requirements

--- pages/home_page.py
+++ pages/home_page.py
@@ -7,7 +7,11 @@ class HomePage(BasePage):
     osher_cohen=(By.XPATH,'//*[@id="search-results-container"]/div/div[2]/div/div[2]/div/a/p')
     sell_tickets=(By.XPATH,'//*[@id="root"]/div[1]/div/nav/p')
     hero_banner=(By.XPATH,'//*[@id="root"]/div[2]/div[1]')
-
+    theatre=(By.XPATH,'//*[@id="root"]/div[2]/div[2]/div/div/div[2]/a[1]')
+    music=(By.XPATH,'//*[@id="root"]/div[2]/div[2]/div/div/div[2]/a[2]')
+    sport=(By.XPATH,'//*[@id="root"]/div[2]/div[2]/div/div/div[2]/a[3]')
+    standup = (By.XPATH, '//*[@id="root"]/div[2]/div[2]/div/div/div[2]/a[4]')
+    kids = (By.XPATH, '//*[@id="root"]/div[2]/div[2]/div/div/div[2]/a[5]')
 
     def click_login(self):
         self.click_element(self.login_btn)
@@ -19,7 +23,19 @@ class HomePage(BasePage):
     def click_sell(self):
         self.click_element(self.sell_tickets)
 
+    def click_theatre(self):
+        self.click_element(self.theatre)
 
+    def click_music(self):
+        self.click_element(self.music)
 
+    def click_sport(self):
+        self.click_element(self.sport)
+
+    def click_standup(self):
+        self.click_element(self.standup)
+
+    def click_kids(self):
+        self.click_element(self.kids)
 
 
