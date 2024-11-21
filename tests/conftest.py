9f20345596983a62ebb2f9366fd3f448a358d8f8
Nelly <nellylisak6@gmail.com>
2024-11-21 12:47:39 +0200

    added firfox

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,14 +2,24 @@
 import  os
 import pytest
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.firefox.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 
 
 os.environ['WDM_LOCAL'] = '1'
 
-
+# @pytest.fixture(scope="session")
+# def driver():
+#     firefox_options=webdriver.FirefoxOptions()
+#     firefox_service=Service(GeckoDriverManager().install())
+#     driver=webdriver.Firefox(service=firefox_service,options=firefox_options)
+#     driver.maximize_window()
+#     yield driver
+#     driver.quit()
 
 @pytest.fixture(scope="session")
 def driver():
